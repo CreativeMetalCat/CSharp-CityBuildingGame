805cb0edd45feaf9bd3c46f2c21241a6e70fe0ae
MetalCat <36876492+CreativeMetalCat@users.noreply.github.com>
2018-04-21 17:54:27 +1000

    Display parts(Windows Form)

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace GameToWorkWith
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.BuildGroup = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.BuildGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "House";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(87, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Farm";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(164, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Pump-house";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // BuildGroup
+            // 
+            this.BuildGroup.Controls.Add(this.label1);
+            this.BuildGroup.Controls.Add(this.button4);
+            this.BuildGroup.Controls.Add(this.button5);
+            this.BuildGroup.Controls.Add(this.label4);
+            this.BuildGroup.Controls.Add(this.label3);
+            this.BuildGroup.Controls.Add(this.label2);
+            this.BuildGroup.Controls.Add(this.button3);
+            this.BuildGroup.Controls.Add(this.button2);
+            this.BuildGroup.Controls.Add(this.button1);
+            this.BuildGroup.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BuildGroup.Location = new System.Drawing.Point(671, -1);
+            this.BuildGroup.Name = "BuildGroup";
+            this.BuildGroup.Size = new System.Drawing.Size(246, 455);
+            this.BuildGroup.TabIndex = 1;
+            this.BuildGroup.TabStop = false;
+            this.BuildGroup.Text = "Buildings";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(4, 403);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 27);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Load";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(165, 403);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 27);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Save";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 22);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 408);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 450);
+            this.Controls.Add(this.BuildGroup);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.BuildGroup.ResumeLayout(false);
+            this.BuildGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox BuildGroup;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GameToWorkWith
+{
+    public partial class Form1 : Form
+    {
+        public House house;
+        public PumpingStation pump;
+        public Farm farm;
+        Game game;
+        Buildings building = Buildings.None;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            game = new Game(CreateGraphics());
+            Thread tr = new Thread(game.Run);
+            tr.IsBackground = true;
+            tr.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            building = Buildings.House;
+            game.pen.Color = Color.Brown;
+            game.People += 10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            building = Buildings.Farm;
+            game.pen.Color = Color.Green;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            building = Buildings.Pump;
+            game.pen.Color = Color.Aqua;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            label1.Text = (e.X + "  " + e.Y).ToString();
+            if (e.Button == MouseButtons.Right)
+            {
+                building = Buildings.None;
+            }
+            else
+            {
+                bool temp = false;
+                if (building != Buildings.None)
+                {
+                    //for (int i = 0; i < game.tiles.Count; i++)
+                    //{
+                    //    if (game.tiles[i] is Building)
+                    //    {
+                    //        for (int u = 0; u < 20; u++)
+                    //        {
+                    //            if (e.X == game.tiles[i].block.X + u)
+                    //            {
+                    //                temp = true;
+                    //            }
+                    //        }
+                    //        for (int u = 0; u < 20; u++)
+                    //        {
+                    //            if (e.Y == game.tiles[i].block.Y + u)
+                    //            {
+                    //                temp = true;
+                    //            }
+                    //        }
+                    //    }
+                    //}
+                    for(int i=0;i<game.tiles.Count;i++)
+                    {
+                        if (game.tiles[i].block.Contains(new PointF(e.X, e.Y)))
+                        {
+                            temp = true;
+                        }
+                    }
+                    if (temp != true)
+                    {
+                        PointF point = new PointF(e.X, e.Y);
+                        game.graphics.FillRectangle(game.pen.Brush, e.X, e.Y, 40, 40);
+                        if (building == Buildings.Farm)
+                        {
+                            game.tiles.Add(new Farm(100, 500, point, Color.Green, 5, GameToWorkWith.Size.Normal,1));
+                        }
+                        if (building == Buildings.House)
+                        {
+                            game.tiles.Add(new House(10, point, Color.Brown, GameToWorkWith.Size.Normal,1));
+                        }
+                        if (building == Buildings.Pump)
+                        {
+                            game.tiles.Add(new PumpingStation(100, 500, point, Color.Aqua, 5, GameToWorkWith.Size.Normal,1));
+                        }
+                    }
+                }
+                label2.Text = game.CountBuildings().ToString();
+                label3.Text = game.Water.ToString();
+                label4.Text = game.Food.ToString();
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            game.Dispose();
+            Dispose();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           game.Save();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            game.Load(openFileDialog1.FileName);
+        }
+    }
+}
